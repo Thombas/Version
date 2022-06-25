a754164c47abf1fb3c7b5920444e744ad74cc997
Thomas Fielding <thomas.fielding@affinityagency.co.uk>
2022-06-25 14:32:11 +0100

    Organised the version service file

--- src/Services/VersionService.php
+++ src/Services/VersionService.php
@@ -21,120 +21,67 @@ class VersionService
     }
 
     /**
-     * Get the root where version logs are stored
+     * Fetch the version number as a complete unit or specified partial
+     *
+     * @param ?string $version The version to increment if fetching future (Major, Minor or Patch)
      *
      * @return string
      */
-    public function getRoot(): string
-    {
-        return $this->root;
-    }
-
-    /**
-     * Get the full version number of the current application
-     *
-     * @param ?string $version The current number to iterate (Major, Minor or Patch)
-     *
-     * @return string
-     */
-    public function getVersionNumber(
+    protected function buildVersionNumber(
         ?string $version = null
     ): string {
-        return implode('.', [
-            $this->getMajorVersionNumber($version),
-            $this->getMinorVersionNumber($version),
-            $this->getPatchVersionNumber($version)
-        ]);
-    }
+        $logs = $this->getLogs();
 
-    /**
-     * Get the current/future major version number of the application
-     *
-     * @param ?string $version The version to increment if fetching future (Major, Minor or Patch)
-     *
-     * @return int
-     */
-    public function getMajorVersionNumber(
-        ?string $version = null
-    ): int {
-        $iteration = $this->buildVersionNumber('major');
+        $major = 0;
+        $minor = 0;
+        $patch = 0;
 
-        switch ($version) {
-            case 'major':
-                return ($iteration + 1);
-                break;
-            default:
-                return $iteration;
-                break;
+        foreach ($logs as $log) {
+            switch ($log->type) {
+                case 'major':
+                    $major++;
+                    $minor = 0;
+                    $patch = 0;
+                    break;
+                case 'minor':
+                    $minor++;
+                    $patch = 0;
+                    break;
+                default:
+                    $patch++;
+                    break;
+            }
         }
+
+        return $version
+            ? ${$version}
+            : implode('.', [
+                $major,
+                $minor,
+                $patch
+            ]);
     }
 
     /**
-     * Get the current/future minor version number of the application
+     * Check that the provided directory exists (and if not then create it)
      *
-     * @param ?string $version The version to increment if fetching future (Major, Minor or Patch)
+     * @param string $directory The directory to scan for
      *
-     * @return int
+     * @return void
      */
-    public function getMinorVersionNumber(
-        ?string $version = null
-    ): int {
-        $iteration = $this->buildVersionNumber('minor');
+    protected function checkDirExists($directory): void
+    {
+        // Break the directory into steps/segments
+        $steps = explode('/', $directory);
 
-        switch ($version) {
-            case 'major':
-                return 0;
-                break;
-            case 'minor':
-                return ($iteration + 1);
-                break;
-            default:
-                return $iteration;
-                break;
+        // Loop through and build each step of the directory
+        foreach ($steps as $key => $step) {
+            $glue = implode('/', array_slice($steps, 0, $key + 1));
+
+            if ($glue && !is_dir($glue)) {
+                mkdir($glue);
+            }
         }
-    }
-
-    /**
-     * Get the current/future patch version number of the application
-     *
-     * @param ?string $version The version to increment if fetching future (Major, Minor or Patch)
-     *
-     * @return int
-     */
-    public function getPatchVersionNumber(
-        ?string $version = null
-    ): int {
-        $iteration = $this->buildVersionNumber('patch');
-
-        switch ($version) {
-            case 'major':
-                return 0;
-                break;
-            case 'minor':
-                return 0;
-                break;
-            case 'patch':
-                return ($iteration + 1);
-                break;
-            default:
-                return $iteration;
-                break;
-        }
-    }
-
-    /**
-     * Fetch logs that come under the given branch id
-     *
-     * @param string $git The git branch hash/id
-     *
-     * @return array
-     */
-    public function getLogsByBranchId(
-        string $git
-    ): array {
-        return array_filter($this->getLogs(), function ($log) use ($git) {
-            return $log->branch_id == $git;
-        });
     }
 
     /**
@@ -220,66 +167,119 @@ class VersionService
     }
 
     /**
-     * Check that the provided directory exists (and if not then create it)
+     * Fetch logs that come under the given branch id
      *
-     * @param string $directory The directory to scan for
+     * @param string $git The git branch hash/id
      *
-     * @return void
+     * @return array
      */
-    protected function checkDirExists($directory): void
-    {
-        // Break the directory into steps/segments
-        $steps = explode('/', $directory);
+    public function getLogsByBranchId(
+        string $git
+    ): array {
+        return array_filter($this->getLogs(), function ($log) use ($git) {
+            return $log->branch_id == $git;
+        });
+    }
 
-        // Loop through and build each step of the directory
-        foreach ($steps as $key => $step) {
-            $glue = implode('/', array_slice($steps, 0, $key + 1));
+    /**
+     * Get the current/future major version number of the application
+     *
+     * @param ?string $version The version to increment if fetching future (Major, Minor or Patch)
+     *
+     * @return int
+     */
+    public function getMajorVersionNumber(
+        ?string $version = null
+    ): int {
+        $iteration = $this->buildVersionNumber('major');
 
-            if ($glue && !is_dir($glue)) {
-                mkdir($glue);
-            }
+        switch ($version) {
+            case 'major':
+                return ($iteration + 1);
+                break;
+            default:
+                return $iteration;
+                break;
         }
     }
 
     /**
-     * Fetch the version number as a complete unit or specified partial
+     * Get the current/future minor version number of the application
      *
      * @param ?string $version The version to increment if fetching future (Major, Minor or Patch)
      *
+     * @return int
+     */
+    public function getMinorVersionNumber(
+        ?string $version = null
+    ): int {
+        $iteration = $this->buildVersionNumber('minor');
+
+        switch ($version) {
+            case 'major':
+                return 0;
+                break;
+            case 'minor':
+                return ($iteration + 1);
+                break;
+            default:
+                return $iteration;
+                break;
+        }
+    }
+
+    /**
+     * Get the current/future patch version number of the application
+     *
+     * @param ?string $version The version to increment if fetching future (Major, Minor or Patch)
+     *
+     * @return int
+     */
+    public function getPatchVersionNumber(
+        ?string $version = null
+    ): int {
+        $iteration = $this->buildVersionNumber('patch');
+
+        switch ($version) {
+            case 'major':
+                return 0;
+                break;
+            case 'minor':
+                return 0;
+                break;
+            case 'patch':
+                return ($iteration + 1);
+                break;
+            default:
+                return $iteration;
+                break;
+        }
+    }
+
+    /**
+     * Get the root where version logs are stored
+     *
      * @return string
      */
-    protected function buildVersionNumber(
+    public function getRoot(): string
+    {
+        return $this->root;
+    }
+
+    /**
+     * Get the full version number of the current application
+     *
+     * @param ?string $version The current number to iterate (Major, Minor or Patch)
+     *
+     * @return string
+     */
+    public function getVersionNumber(
         ?string $version = null
     ): string {
-        $logs = $this->getLogs();
-
-        $major = 0;
-        $minor = 0;
-        $patch = 0;
-
-        foreach ($logs as $log) {
-            switch ($log->type) {
-                case 'major':
-                    $major++;
-                    $minor = 0;
-                    $patch = 0;
-                    break;
-                case 'minor':
-                    $minor++;
-                    $patch = 0;
-                    break;
-                default:
-                    $patch++;
-                    break;
-            }
-        }
-
-        return $version
-            ? ${$version}
-            : implode('.', [
-                $major,
-                $minor,
-                $patch
-            ]);
+        return implode('.', [
+            $this->getMajorVersionNumber($version),
+            $this->getMinorVersionNumber($version),
+            $this->getPatchVersionNumber($version)
+        ]);
     }
 }
